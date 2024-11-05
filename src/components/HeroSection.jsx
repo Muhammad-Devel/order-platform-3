624f90e1cea5742669e969d31757e6930b0c191b
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-11-05 15:49:17 +0500

    heroSection and Navbar section edited

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
 
   return (
     
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="relative isolate px-6 pt-8 lg:px-8">
         <div
           aria-hidden="true"
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -23,7 +23,7 @@ export default function HeroSection() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        
+
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
 
           <div className="text-center">
