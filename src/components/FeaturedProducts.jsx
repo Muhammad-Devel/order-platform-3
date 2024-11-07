8379ade44630e2f92120571b447ef5a30c8492b9
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-11-08 00:44:48 +0500

    dfsdf

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -4,10 +4,10 @@ import ProductCard from "./ProductCard";
 
 const FeaturedProducts = ({ products }) => {
   return (
-    <section className="py-8 bg-gray-100">
+    <section className="py-4 bg-gray-100">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-6">
-          Featured Products
+        <h2 className="text-3xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-3">
+          Tavsiya qilamiz
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 sm:gap-6 md:gap-6 lg:gap-6">
           {products.map((product) => (
